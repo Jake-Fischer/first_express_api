4e32b77c7dbb9963615b6b63177de7bdf8cb5350
Jake-Fischer <Taylormade123!>
2021-04-22 20:43:34 -0500

    setting up heroku

--- hello-vue/src/main.js
+++ hello-vue/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import HelloAPI from '@/services/HelloAPI'
 
-let app = createApp(App).mount('#app')
+let app = createApp(App)
 
 app.config.globalProperties.$hello_api = HelloAPI
 
